0fae65445b57f4a9d6e15d215496fe94a9fde406
Charles AGBO <agbo.charles85@gmail.com>
2024-04-19 11:00:52 +0200

    Rajout des opérations somme et produit

--- src/main/java/labJunit/app/Calculatrice.java
+++ src/main/java/labJunit/app/Calculatrice.java
@@ -6,4 +6,12 @@ public class Calculatrice {
 		System.out.println("Bienvenue");
 	}
 
+	public double somme(double a, double b) {
+		return a + b;
+	}
+
+	public double produit(double a, double b) {
+		return a * b;
+	}
+
 }
--- src/main/java/labJunit/app/Main.java
+++ src/main/java/labJunit/app/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		Calculatrice c = new Calculatrice();
 
+		double s = c.somme(2, 3);
+		System.out.println("La somme de 2 et 3 est : " + s); // doit afficher "La somme de 2 et 3 est : 5.0"
+
+		double p = c.produit(2, 3);
+		System.out.println("Le produit de 2 et 3 est : " + p); // doit afficher "Le produit de 2 et 3 est : 6.0"
+
 	}
 
 }
